5dfeb8cf7d7da8c39bc1bfd5cb54bb9b8670fe61
Jessicka Mucy <jessickamc@gmail.com>
2023-12-07 19:02:36 +0100

    modif meteo 1

--- meteo.js
+++ meteo.js
@@ -5,14 +5,49 @@ var success = function(data) {
     //element.innerHTML = "La temperature est de " + data.main.temp;
 }
 
+function handler(event)  {
+    var response  = event.response;
+    var headers  = response.headers;
+
+    // If Access-Control-Allow-Origin CORS header is missing, add it.
+    // Since JavaScript doesn't allow for hyphens in variable names, we use the dict["key"] notation.
+    if (!headers['access-control-allow-origin']) {
+        headers['access-control-allow-origin'] = {value: "*"};
+        console.log("Access-Control-Allow-Origin was missing, adding it now.");
+    }
+
+    return response;
+}
 
 function callAPI() {
-    var api = "http://api.weatherstack.com/current";
-    var access_key = 'cc0ec2c6072259e9e860d22017ba42c9';
-    var query = 'Niort';
 
-    var url  = api + "?access_key=" + access_key + "&query=" + query
+   
+    const settings = {
+        async: true,
+        crossDomain: true,
+        url: 'https://forecast9.p.rapidapi.com/',
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': 'c2fdb57519mshcec056a2d3b9592p10e9bfjsnfb6230b5d39d',
+            'X-RapidAPI-Host': 'forecast9.p.rapidapi.com'
+        }
+    };
+    
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+    });
 
+
+   /* var url = "https://example.com/api/current";
+
+    // Paramètres de la requête
+    var params = {
+        access_key: "cc0ec2c6072259e9e860d22017ba42c9",
+        query: "Niort"
+    };
+
+    //var url="https://api.openweathermap.org/data/2.5/weather?q=Niort&appid=3b062c74131c1bd300be8490bd7f83c8&units=metric"
+    var url="origin:https://example.com/api/current?access_key=cc0ec2c6072259e9e860d22017ba42c9&query=Niort"
 
     $.get(url, success).done(function() {
         //alert( "second success" );
@@ -22,5 +57,5 @@ function callAPI() {
       })
       .always(function() {
         //alert( "finished" );
-      });
+      });*/
 }
